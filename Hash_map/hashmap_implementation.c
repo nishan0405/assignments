98530fbf38c5230a771bda789c96a8d1133a7d20
nishan0405 <nishanar3@gmail.com>
2025-11-24 19:12:16 +0530

    Refactor hashmap implementation

--- Hash_map/hashmap_implementation.c
+++ Hash_map/hashmap_implementation.c
@@ -12,6 +12,43 @@ struct Node {
 
 struct Node *hashtabel[length];
 
+int hash(char *key);
+struct Node* search1(char *key);
+void put(char *key, int value);
+void delet(char *key);
+int search(char *key);
+void get(char *key);
+void display();
+int main()
+{
+    for (int i = 0; i < length; i++)
+        hashtabel[i] = NULL;
+
+    put("apple", 40);
+    put("elppa", 50);
+    put("banana", 20);
+    put("grapes", 90);
+    put("orange", 60);
+
+    display();
+
+    if (search("apple"))
+        printf("Key found\n");
+    else
+        printf("Key not found\n");
+
+    delet("apple");
+    display();
+    
+    get("banana");
+    put("banana", 50);
+    get("banana");
+
+    display();
+
+    return 0;
+}
+
 int hash(char *key)
 {
     int sum = 0;
@@ -20,17 +57,17 @@ int hash(char *key)
 
     return sum % length;
 }
+
 struct Node *search1(char *key)
 {
-    unsigned int index=hash(key);
-    struct Node *temp=hashtabel[index];
-    while(temp!=NULL)
+    unsigned int index = hash(key);
+    struct Node *temp = hashtabel[index];
+    
+    while (temp != NULL)
     {
-        if(strcmp(temp->str,key)==0)
-        {
+        if (strcmp(temp->str, key) == 0)
             return temp;
-        }
-        temp=temp->next;
+        temp = temp->next;
     }
     return NULL;
 }
@@ -39,16 +76,16 @@ void put(char *key, int value)
 {
     unsigned int index = hash(key);
 
-    struct Node *newnode = (struct Node*)malloc(sizeof(struct Node));
     struct Node *node = search1(key);
 
     if (node != NULL)
     {
         node->data = value;
         printf("Value updated\n");
-        return; 
+        return;
     }
 
+    struct Node *newnode = (struct Node*)malloc(sizeof(struct Node));
     strcpy(newnode->str, key);
     newnode->data = value;
     newnode->next = hashtabel[index];
@@ -108,17 +145,14 @@ int search(char *key)
 
     return 0;
 }
+
 void get(char *key)
 {
-    struct Node *temp=search1(key);
-    if(temp !=NULL)
-    {
-        printf("the value at the key is %d\n",temp->data);
-        return;
-    }
-    else{
+    struct Node *temp = search1(key);
+    if (temp != NULL)
+        printf("the value at the key is %d\n", temp->data);
+    else
         printf("the key not found\n");
-    }
 }
 
 void display()
@@ -137,30 +171,4 @@ void display()
     }
 }
 
-int main()
-{
-    for (int i = 0; i < length; i++)
-        hashtabel[i] = NULL;
 
-    put("apple", 40);
-    put("elppa",50);
-    put("banana", 20);
-    put("grapes", 90);
-    put("orange", 60);
-
-    display();
-
-    if (search("apple"))
-        printf("Key found\n");
-    else
-        printf("Key not found\n");
-
-    delet("apple");
-    display();
-    get("banana");
-    put("banana", 50);
-    get("banana");
-    display();
-
-    return 0;
-}
